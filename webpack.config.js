ac3c8c8d88bafd4fd74e11ddcf99cee2fc431138
Calem Bendell <calem.j.bendell@gmail.com>
2015-12-14 11:15:50 -0800

    fix font loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var webpack = require('webpack');
 
-// postcss plugins
 var cssimport = require('postcss-import');
 var customProperties = require('postcss-custom-properties');
 var autoprefixer = require('autoprefixer-core');
@@ -9,7 +8,7 @@ var cssnested = require('postcss-nested');
 
 module.exports = {
   entry: {
-    app : ['./src/index.js']
+    app: ['./src/index.js']
   },
   output: {
     path: __dirname + '/build/',
@@ -28,18 +27,37 @@ module.exports = {
     })
   ],
   module: {
-    preLoaders: [
-      { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'es6' } }
-    ],
-    loaders: [
-      { test: /\.js|\.tag$/, exclude: /node_modules/, include: /src/, loader: 'babel-loader', query: {modules: 'common'} },
-      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' }
-    ]
+    preLoaders: [{
+      test: /\.tag$/,
+      exclude: /node_modules/,
+      loader: 'riotjs-loader',
+      query: {
+        type: 'es6'
+      }
+    }],
+    loaders: [{
+      test: /bootstrap\/js\//,
+      loader: 'imports?jQuery=jquery'
+    }, {
+      test: /\.js|\.tag$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'babel-loader',
+      query: {
+        modules: 'common'
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader!postcss-loader'
+    }, {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000'
+    }]
   },
   postcss: [cssimport, cssnested, customProperties, autoprefixer, csswring],
   devServer: {
     contentBase: './build/',
-    port: 1339,
+    port: 1338,
     hot: true,
     inline: true
   }
